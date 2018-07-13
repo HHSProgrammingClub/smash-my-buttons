043b4247bd433a17d58c055a072bf4907bcb667c
Catherine Guevara <silveretriever206@gmail.com>
2018-07-12 21:20:05 -0700

    whoa fancy comments
    Not sure how long I'll keep this up buuttttt...

--- src/Rect.java
+++ src/Rect.java
@@ -1,8 +1,19 @@
-
+/**
+ * Rectangle class for various uses.
+ * @author Catherine Guevara
+ */
 public class Rect 
 {
+	/** Coordinates of rectangle, width, and height*/
 	public float x, y, w, h;
 
+	/** 
+	 * Constructor.
+	 * @param p_x x-coordinate (top-left corner)
+	 * @param p_y y-coordinate (top-left corner)
+	 * @param p_w width
+	 * @param p_h height
+	 */
 	public Rect(float p_x, float p_y, float p_w, float p_h)
 	{
 		x = p_x;
@@ -11,7 +22,11 @@ public class Rect
 		h = p_h;
 	}
 	
-	//checks if two Rects overlap each other
+	/**
+	 * Checks if two Rects overlap with each other
+	 * @param p_rect Rect to check collision
+	 * @return whether or not they overlap
+	 */
 	public boolean collidesWith(Rect p_rect)
 	{
 		return (x <= (p_rect.x + p_rect.w) && 
@@ -20,13 +35,20 @@ public class Rect
 				p_rect.y <= (p_rect.y + h));
 	}
 	
+	/**
+	 * Returns the center coordinates of a Rect
+	 * @return Vector2
+	 */
 	public Vector2 getMiddle()
 	{
 		//check me on this -Cathy
 		return new Vector2(x + w/2, y + h/2);
 	}
 	
-	//scales the height and width of the rectangle
+	/**
+	 * Scales a given Rect by a scalar
+	 * @param scalar the scalar
+	 */
 	public void scale(float scalar)
 	{
 		w *= scalar;
