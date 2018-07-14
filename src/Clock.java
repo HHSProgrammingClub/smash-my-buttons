2e1ac773a6d494b1af01145d954557c77b1eabc3
Michael Thompson <capnsword@gmail.com>
2018-07-13 20:37:59 -0700

    Implemented Clock

--- src/Clock.java
+++ src/Clock.java
@@ -1,5 +1,75 @@
 
-public class Clock 
+public class Clock
 {
-	//ticky-tocky stuff goes here
+	private long m_startTime;
+	private long m_pauseTime;
+	private boolean m_isPaused;
+	
+	/**
+	 * Construct a clock. The clock begins at construction.
+	 */
+	Clock()
+	{
+		m_startTime = System.nanoTime();
+		m_isPaused = false;
+	}
+	
+	/**
+	 * Get elapse from the start of this clock to now in seconds.
+	 * @return Elapse in seconds
+	 */
+	public float getElapse()
+	{
+		long elapseNano = System.nanoTime() - m_startTime;
+		return (float)elapseNano*1e-9f; // Convert to seconds
+	}
+	
+	/**
+	 * Start the clock. Use getElapse() to get the time.
+	 */
+	public void start()
+	{
+		long nowTime = System.nanoTime();
+		if (m_isPaused)
+			m_startTime += nowTime - m_pauseTime;
+		m_isPaused = false;
+	}
+	
+	/**
+	 * Pause and reset the clock to 0.
+	 */
+	public void stop()
+	{
+		pause();
+		restart();
+	}
+	
+	/**
+	 * Pause this clock
+	 */
+	public void pause()
+	{
+		m_pauseTime = System.nanoTime();
+		m_isPaused = true;
+	}
+	
+	/**
+	 * Reset this clock to 0 seconds.
+	 */
+	public void restart()
+	{
+		if (m_isPaused)
+			m_startTime = m_pauseTime; // Distance between start time and pause time becomes 0.
+		else
+			m_startTime = System.nanoTime();
+	}
+	
+	/**
+	 * Check if this clock is paused.
+	 * @return True if paused.
+	 */
+	public boolean isPaused()
+	{
+		return m_isPaused;
+	}
 }
