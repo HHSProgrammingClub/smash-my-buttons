cc34ec7a0105fa266321d2043f1e98bf11228574
Ben Hansen <nopika99@gmail.com>
2018-07-18 23:01:25 -0700

    Environmental beginnings

--- src/Environment.java
+++ src/Environment.java
@@ -1,5 +1,30 @@
+import java.util.ArrayList;
 
-public interface Environment
+import org.dyn4j.dynamics.Body;
+import org.dyn4j.dynamics.World;
+import org.dyn4j.geometry.MassType;
+
+public abstract class Environment
 {
+	public Environment() {}
 	
+	public Environment(World p_world)
+	{
+		setPhysicsWorld(p_world);
+	}
+	
+	public void setPhysicsWorld(World p_world)
+	{
+		m_world = p_world;
+	}
+	
+	private void addTerrainPiece(Body p_piece)
+	{
+		m_terrain.add(p_piece);
+		m_terrain.get(m_terrain.size()).setMassType(MassType.INFINITE);
+	}
+	
+	private World m_world;
+	
+	private ArrayList<Body> m_terrain;
 }
