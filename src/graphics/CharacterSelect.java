b15359ba3b31e03112371c287b1c5cbfc07e13cb
Ben Hansen <nopika99@gmail.com>
2018-07-23 22:34:43 -0700

    Load Script buttons

--- src/graphics/CharacterSelect.java
+++ src/graphics/CharacterSelect.java
@@ -11,6 +11,8 @@ import javax.swing.JComboBox;
 import javax.swing.JLabel;
 
 import characters.Character;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
 
 public class CharacterSelect implements Page
 {
@@ -32,7 +34,7 @@ public class CharacterSelect implements Page
 				FormSpecs.RELATED_GAP_COLSPEC,
 				ColumnSpec.decode("max(10dlu;default):grow"),
 				FormSpecs.RELATED_GAP_COLSPEC,
-				ColumnSpec.decode("right:60dlu"),
+				ColumnSpec.decode("center:60dlu"),
 				ColumnSpec.decode("right:16dlu"),},
 			new RowSpec[] {
 				FormSpecs.DEFAULT_ROWSPEC,
@@ -48,10 +50,25 @@ public class CharacterSelect implements Page
 				FormSpecs.RELATED_GAP_ROWSPEC,
 				FormSpecs.DEFAULT_ROWSPEC,}));
 		
+		JLabel lblAi = new JLabel("AI 1");
+		lblAi.setHorizontalAlignment(SwingConstants.CENTER);
+		m_panel.add(lblAi, "2, 4");
+		
+		JLabel lblAi_1 = new JLabel("AI 2");
+		m_panel.add(lblAi_1, "6, 4");
+		
+		JButton btnLoadScript = new JButton("Load Script");
+		m_panel.add(btnLoadScript, "2, 6");
+		
+		JButton btnLoadScript_1 = new JButton("Load Script");
+		m_panel.add(btnLoadScript_1, "6, 6");
+		
 		JLabel lblPlayer = new JLabel("Player 1");
+		lblPlayer.setHorizontalAlignment(SwingConstants.CENTER);
 		m_panel.add(lblPlayer, "2, 10");
 		
 		JLabel lblPlayer_1 = new JLabel("Player 2");
+		lblPlayer_1.setHorizontalAlignment(SwingConstants.CENTER);
 		m_panel.add(lblPlayer_1, "6, 10");
 																   //gui editor yells at me if i don't do this for now
 		JComboBox<String> characterSelector1 = new JComboBox<String>(/*Character.characterNames*/);
