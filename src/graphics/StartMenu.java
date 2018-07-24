af8ff00e48d9b5c5d8b22aa2a227d66f60bab47c
Ben Hansen <nopika99@gmail.com>
2018-07-23 21:05:36 -0700

    Start menu no longer extends JPanel

--- src/graphics/StartMenu.java
+++ src/graphics/StartMenu.java
@@ -11,10 +11,25 @@ import com.jgoodies.forms.layout.RowSpec;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class StartMenu extends JPanel implements Page
+public class StartMenu implements Page
 {
+	private JPanel m_panel;
 	public StartMenu()
 	{
+		setUpPanel();
+	}
+	
+	private void setUpPanel()
+	{
+		m_panel.setLayout(new FormLayout(new ColumnSpec[]
+				{
+				ColumnSpec.decode("197px"),
+				ColumnSpec.decode("57px"),},
+			new RowSpec[] {
+				RowSpec.decode("210px"),
+				RowSpec.decode("23px"),
+				FormSpecs.LABEL_COMPONENT_GAP_ROWSPEC,
+				RowSpec.decode("23px"),}));
 		
 		JButton startButton = new JButton("Start");
 		startButton.addActionListener(new ActionListener()
@@ -25,16 +40,6 @@ public class StartMenu extends JPanel implements Page
 				
 			}
 		});
-		setLayout(new FormLayout(new ColumnSpec[]
-				{
-				ColumnSpec.decode("197px"),
-				ColumnSpec.decode("57px"),},
-			new RowSpec[] {
-				RowSpec.decode("210px"),
-				RowSpec.decode("23px"),
-				FormSpecs.LABEL_COMPONENT_GAP_ROWSPEC,
-				RowSpec.decode("23px"),}));
-		add(startButton, "2, 2, center, center");
 		
 		JButton quitButton = new JButton("Quit");
 		quitButton.addActionListener(new ActionListener()
@@ -44,13 +49,14 @@ public class StartMenu extends JPanel implements Page
 				//quit
 			}
 		});
-		add(quitButton, "2, 4, center, center");
 		
+		m_panel.add(startButton, "2, 2, center, center");
+		m_panel.add(quitButton, "2, 4, center, center");
 	}
 
 	@Override
 	public JComponent getComponent()
 	{
-		return this;
+		return m_panel;
 	}
 }
