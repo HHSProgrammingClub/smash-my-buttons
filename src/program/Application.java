3b23f0c252920cacbc6a7fde86d2a63f9491e310
Ben Hansen <nopika99@gmail.com>
2018-07-26 21:14:06 -0700

    Game loop move to battle class

--- src/program/Application.java
+++ src/program/Application.java
@@ -1,24 +1,15 @@
 package program;
-import java.awt.Color;
 
-import graphics.DebugBox;
 import graphics.GUI;
-import graphics.IntRect;
-import graphics.RenderList;
 import graphics.Renderer;
-import graphics.Sprite;
 import graphics.StartMenu;
-import graphics.Texture;
 
 /**
  * Smash-Bros style game that teaches people how to code in Python
  * @authors Michael Thompson, Catherine Guevara, Benhardt Hansen, Kevin Yang 
  */
 public class Application 
-{
-	private static boolean running = true;
-	private static boolean visibleHitboxes = true;
-	
+{	
 	public static void main(String[] args)
 	{
 		// Test Controller
@@ -33,65 +24,5 @@ public class Application
 		
 		StartMenu start = new StartMenu(gui);
 		gui.setPage(start);
-	
-		
-		RenderList renderList = new RenderList();
-		
-		//test sprites
-		Texture tex1 = new Texture();
-		tex1.openResource("resources/images/birboi");
-		
-		Sprite sprite = new Sprite(tex1, "idle");
-		renderList.addDrawable(sprite);
-		
-		Texture tex2 = new Texture();
-		tex2.openResource("resources/images/coffee");
-		
-		Sprite sprite2 = new Sprite(tex2, "default");
-		renderList.addDrawable(sprite2);
-		
-		//debug
-		//DebugDrawer debugger = new DebugDrawer(world);
-		
-		Clock gameClock = new Clock();
-		float delta = 0;
-		
-		//game loop: should move somewhere else and only start
-		//once the battle itself starts
-		while(running)
-		{ 
-			//calculate delta
-			delta = gameClock.getElapse();
-			gameClock.restart();
-			
-			//clear the buffer
-			renderer.clear();
-			
-			//update the world
-			//draw sprites
-			renderList.draw(renderer);
-			
-			//debug
-			if(visibleHitboxes);
-				//debugger.draw(renderer)
-			
-			//display the current frame
-			renderer.display();
-			
-			//delay
-			try {
-				long totalNanos = 16666666 - (int)(gameClock.getElapse()*1e9f);
-				if(totalNanos > 0)
-				{
-					int nanos = (int) (totalNanos % 1000000);
-					long milis = (totalNanos - nanos) / 1000000;
-					Thread.sleep(milis, (int)nanos);
-				}
-				//this one's simpler
-				//Thread.sleep(16, 666666);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
 	}
 }
--- src/program/Battle.java
+++ src/program/Battle.java
@@ -1,23 +1,34 @@
 package program;
+
 import characters.*;
+import graphics.DebugDrawer;
+import graphics.RenderList;
+import graphics.Renderer;
+import graphics.Sprite;
+import graphics.Texture;
 
 import java.util.ArrayList;
 import org.dyn4j.dynamics.Body;
 import org.dyn4j.dynamics.World;
+
 public class Battle
 {
 	private ArrayList<Hitbox> m_hitboxes;
 	// List of active moves, in order to keep track of lasting effects.
 	private ArrayList<Move> m_activeMoves;
 	private Environment m_env;
+
+	private boolean m_visibleHitboxes = true;
 	
 	private CharacterController[] m_charControllers = {
 		new AIController(),
 		new AIController()
 	};
 	
-	public Battle() {
-		for(int i = 0; i < m_charControllers.length; i++) {
+	public Battle()
+	{
+		for(int i = 0; i < m_charControllers.length; i++)
+		{
 			m_charControllers[i].setCharacter(new GeorgeTheGlassCutter());
 			m_env.getPhysicsWorld().addBody(
 					m_charControllers[i].getCharacter().getBody());
@@ -55,6 +66,11 @@ public class Battle
 		return m_charControllers.length;
 	}
 	
+	public void setVisibleHitboxes(boolean p_visible)
+	{
+		m_visibleHitboxes = p_visible;
+	}
+	
 	public void update(float p_delta)
 	{
 		m_env.getPhysicsWorld().updatev((double)(p_delta));
@@ -66,6 +82,68 @@ public class Battle
 			if(!m_hitboxes.get(i).alive())
 			{
 				m_hitboxes.remove(i);
+			}
+		}
+	}
+	
+	public void gameLoop(Renderer p_renderer)
+	{
+		RenderList renderList = new RenderList();
+		
+		//test sprites
+		Texture tex1 = new Texture();
+		tex1.openResource("resources/images/birboi");
+		
+		Sprite sprite = new Sprite(tex1, "idle");
+		renderList.addDrawable(sprite);
+		
+		Texture tex2 = new Texture();
+		tex2.openResource("resources/images/coffee");
+		
+		Sprite sprite2 = new Sprite(tex2, "default");
+		renderList.addDrawable(sprite2);
+		
+		//debug
+		DebugDrawer debugger = new DebugDrawer(m_env.getPhysicsWorld());
+		
+		Clock gameClock = new Clock();
+		float delta = 0;
+		
+		//game loop: should move somewhere else and only start
+		//once the battle itself starts
+		while(true)
+		{ 
+			//calculate delta
+			delta = gameClock.getElapse();
+			gameClock.restart();
+			
+			//clear the buffer
+			p_renderer.clear();
+			
+			//update the world
+			//draw sprites
+			renderList.draw(p_renderer);
+			
+			//debug
+			if(m_visibleHitboxes);
+				debugger.draw(p_renderer);
+			
+			//display the current frame
+			p_renderer.display();
+			
+			//delay
+			try {
+				long totalNanos = 16666666 - (int)(gameClock.getElapse()*1e9f);
+				if(totalNanos > 0)
+				{
+					int nanos = (int) (totalNanos % 1000000);
+					long milis = (totalNanos - nanos) / 1000000;
+					Thread.sleep(milis, (int)nanos);
+				}
+				//this one's simpler
+				//Thread.sleep(16, 666666);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
 		}
 	}
