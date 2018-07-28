5981cc6ab38b8cda1817c530ad3a60037d5e9d3c
Catherine Guevara <silveretriever206@gmail.com>
2018-07-28 06:28:26 -0700

    Added Sound class
    Note everything needs to be .wav

---
+++ src/program/Sound.java
@@ -0,0 +1,16 @@
+package program;
+
+import java.applet.Applet;
+import java.applet.AudioClip;
+import sun.audio.*;
+import java.io.*;
+
+public class Sound 
+{
+	//public static final AudioClip NAME = Applet.newAudioClip(Sound.class.getResource("resources/sfx/name.wav")); 
+	   /*
+	    * Sound.NAME.play();
+	    * Sound.NAME.loop();
+	    * Sound.NAME.stop();
+	    */
+}
