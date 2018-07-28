584c2fa5332a518cdc62a0d47a1aed8c463398e9
lgtyqz <kevinlyang01@gmail.com>
2018-07-28 15:39:55 -0700

    George is reborn correctly

--- src/characters/GeorgeTheGlassCutter.java
+++ src/characters/GeorgeTheGlassCutter.java
@@ -4,6 +4,7 @@ import org.dyn4j.dynamics.Body;
 import org.dyn4j.dynamics.BodyFixture;
 import org.dyn4j.geometry.MassType;
 import org.dyn4j.geometry.Rectangle;
+import org.dyn4j.geometry.Transform;
 
 import program.Battle;
 
@@ -11,17 +12,23 @@ import program.Battle;
 
 public class GeorgeTheGlassCutter extends Character
 {	
+	private static double position = 0;
 	public GeorgeTheGlassCutter() 
 	{
 		Body tushie = new Body();
 		// Make it non-rotating
-		tushie.setMass(MassType.FIXED_ANGULAR_VELOCITY);
+		tushie.setMass(MassType.NORMAL);
+		Transform t = new Transform();
+		t.setTranslation(position, 100);
+		tushie.setTransform(t);
+		position += 100;
 		// Add the collision fixture
-		BodyFixture hurtbox = new BodyFixture(new Rectangle(64, 64)); //this will be very, very big
-		hurtbox.setDensity(20);
+		/*BodyFixture hurtbox = new BodyFixture(new Rectangle(64, 64)); //this will be very, very big
+		hurtbox.setDensity(1);
 		hurtbox.setFriction(0.5);
-		hurtbox.setRestitution(0.9);
-		tushie.addFixture(hurtbox);
+		hurtbox.setRestitution(0.9);*/
+		tushie.addFixture(new Rectangle(64, 64));
+		//tushie.setLinearVelocity(2, 5);
 		setBody(tushie);
 	}
 	
