2eeb6fbd5dc86584e3ee5e61ea8f86965cc9e612
Michael Thompson <capnsword@gmail.com>
2018-07-31 13:20:31 -0700

    Cleaned up GroundListener

--- src/program/GroundListener.java
+++ src/program/GroundListener.java
@@ -1,27 +1,13 @@
 package program;
-import org.dyn4j.dynamics.Body;
-import org.dyn4j.dynamics.contact.ContactListener;
+
+import org.dyn4j.dynamics.contact.ContactAdapter;
 import org.dyn4j.dynamics.contact.ContactPoint;
-import org.dyn4j.dynamics.contact.PersistedContactPoint;
-import org.dyn4j.dynamics.contact.SolvedContactPoint;
 
 import characters.Character;
 import stages.TerrainPiece;
 
-public class GroundListener implements ContactListener {
-	@Override
-	//The important one
-	public void sensed(ContactPoint point) {
-
-	}
-	@Override
-	public boolean preSolve(ContactPoint point) { return true; }
-	@Override
-	public void postSolve(SolvedContactPoint point) {}
-	@Override
-	public boolean persist(PersistedContactPoint point) { return true; }
-	@Override
-	public void end(ContactPoint point) {}
+public class GroundListener extends ContactAdapter
+{
 	@Override
 	public boolean begin(ContactPoint point)
 	{
@@ -32,11 +18,9 @@ public class GroundListener implements ContactListener {
 			// Veeeeery rudimentary ground collision algorithm
 			// Essentially, tests whether it has collided with the top half
 			// of the TerrainPiece
-			if(userdata.second.getBody().getWorldCenter().y < point.getPoint().y /*&& // it seems to do just fine without the "change in position" check
-					c.getBody().getChangeInPosition().y > 0*/) 
+			if(userdata.second.getBody().getWorldCenter().y < point.getPoint().y)
 			{
 				userdata.second.resetJump();
-				System.out.println("reset");
 			}
 		}
 		return true;
