6d0004329a8785ed69216bd4e5f35bfc4706a3f1
Catherine Guevara <silveretriever206@gmail.com>
2018-07-31 22:48:06 -0700

    Replaced jump animation tweaks

--- src/characters/Character.java
+++ src/characters/Character.java
@@ -230,8 +230,7 @@ public abstract class Character implements Drawable
 		{
 			m_body.applyImpulse(jumpImpulse);
 			m_jumped = true;
-			if(!m_jumped)
-				m_sprite.setAnimation("jump_asc");
+			m_sprite.setAnimation("jump_asc");
 		}
 	}
 	
@@ -247,14 +246,16 @@ public abstract class Character implements Drawable
 	{
 		m_moving = true;
 		m_body.applyForce(force_L);
-		m_sprite.setAnimation("run");
+		if(!m_jumped)
+			m_sprite.setAnimation("run");
 	}
 	
 	public void moveRight()
 	{
 		m_moving = true;
 		m_body.applyForce(force_R);
-		m_sprite.setAnimation("run");
+		if(!m_jumped)
+			m_sprite.setAnimation("run");
 	}
 	
 	public abstract void jab();
