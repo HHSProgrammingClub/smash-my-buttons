ce93025ed6792c80169d6db67667adc0d6e4a9cb
Catherine Guevara <silveretriever206@gmail.com>
2018-08-03 17:35:03 -0700

    Edgewardo's jab and smash
    He's probably a bit OP at the moment, but I'll adjust him later. I want
    to get the basis down first.

--- src/characters/Edgewardo.java
+++ src/characters/Edgewardo.java
@@ -1,15 +1,22 @@
 package characters;
 
 import org.dyn4j.dynamics.Body;
+import org.dyn4j.dynamics.BodyFixture;
 import org.dyn4j.geometry.MassType;
 import org.dyn4j.geometry.Rectangle;
 import org.dyn4j.geometry.Transform;
+import org.dyn4j.geometry.Vector2;
 
+import characters.characterStates.CharacterState;
 import graphics.Sprite;
 import graphics.Texture;
+import program.Hitbox;
 
 public class Edgewardo extends Character
 {
+	private static double position = 0;
+	private float length = 1;
+	private float height = 2;
 
 	public Edgewardo()
 	{
@@ -37,12 +44,56 @@ public class Edgewardo extends Character
 		setSprite(sp);
 	}
 	
+	private class JabState extends CharacterState
+	{
+		private Hitbox m_hitbox = new Hitbox();
+		private Rectangle m_rect;
+		private BodyFixture m_fixture;
+		
+		public JabState()
+		{
+			super("jab");
+			
+			m_hitbox.setDuration(0.2f);
+			m_hitbox.setDamage(5);
+			m_hitbox.setHitstun(0.3f);
+			m_hitbox.setBaseKnockback(new Vector2(1.5 * getFacing(), 0));
+			m_hitbox.setScaledKnockback(new Vector2(getFacing(), -1));
+		
+			m_rect = new Rectangle(0.5, 1);
+			m_rect.translate(length + 0.25 * getFacing(), 1);
+			
+			m_fixture = new BodyFixture(m_rect);
+		}
+		
+		protected void init()
+		{
+			addHitbox(m_hitbox);
+			m_hitbox.addToFixture(m_fixture);
+			m_body.addFixture(m_fixture);
+		}
+		
+		public void interrupt()
+		{
+			m_body.removeFixture(m_fixture);
+			removeHitbox(m_hitbox);
+		}
+		
+		public void end()
+		{
+			m_body.removeFixture(m_fixture);
+			removeHitbox(m_hitbox);
+		}
+	};
+	
 	@Override
 	public void jab() 
 	{
-		// TODO Auto-generated method stub
-		m_sprite.setAnimation("jab");
+		interruptStates(new CharacterState("jab", 0.1f));
+		addState(new JabState());
 	}
+	
+	//tilt is kinda weird
 
 	@Override
 	public void tilt() 
@@ -50,12 +101,54 @@ public class Edgewardo extends Character
 		// TODO Auto-generated method stub
 		
 	}
+	
+	private class SmashState extends CharacterState
+	{
+		private Hitbox m_hitbox = new Hitbox();
+		private Rectangle m_rect;
+		private BodyFixture m_fixture;
+		
+		public SmashState()
+		{
+			super("smash");
+			
+			m_hitbox.setDuration(0.4f);
+			m_hitbox.setDamage(15);
+			m_hitbox.setHitstun(0.4f);
+			m_hitbox.setBaseKnockback(new Vector2(0.5 * getFacing(), 0));
+			m_hitbox.setScaledKnockback(new Vector2(4 * getFacing(), -1));
+			
+			m_rect = new Rectangle(0.5, 1);
+			m_rect.translate(length + 0.5 * getFacing(), 1.25);
+		
+			m_fixture = new BodyFixture(m_rect);
+		}
+		
+		protected void init()
+		{
+			addHitbox(m_hitbox);
+			m_hitbox.addToFixture(m_fixture);
+			m_body.addFixture(m_fixture);
+		}
+		
+		public void interrupt()
+		{
+			m_body.removeFixture(m_fixture);
+			removeHitbox(m_hitbox);
+		}
+		
+		public void end()
+		{
+			m_body.removeFixture(m_fixture);
+			removeHitbox(m_hitbox);
+		}
+	};
 
 	@Override
 	public void smash()
 	{
-		// TODO Auto-generated method stub
-		m_sprite.setAnimation("smash");
+		interruptStates(new CharacterState("smash", 0.1f));
+		addState(new SmashState());
 	}
 
 	@Override
