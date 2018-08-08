6751148a6e5c0d4dee11485bef14b5652854c19e
Michael Thompson <capnsword@gmail.com>
2018-08-08 13:16:45 -0700

    Moved wrapper classes outside of AIController

---
+++ src/pythonAI/PyEnemyWrapper.java
@@ -0,0 +1,36 @@
+package pythonAI;
+
+import characters.Character;
+
+class PyEnemyWrapper implements pythonAI.interfaces.EnemyInterface {
+	public Character m_character;
+	
+	public void setCharacter(Character p_character) {
+		m_character = p_character;
+	}
+	
+	@Override
+	public float getX()
+	{
+		// TODO Auto-generated method stub
+		return (float) m_character.getBody().getWorldCenter().x;
+	}
+
+	@Override
+	public float getY()
+	{
+		return (float) m_character.getBody().getWorldCenter().y;
+	}
+	
+	@Override
+	public float getHitstun()
+	{
+		// TODO Auto-generated method stub
+		return m_character.peekState().getTimer();
+	}
+	
+	@Override
+	public int getDamage() {
+		return m_character.getDamage();
+	}
+}
