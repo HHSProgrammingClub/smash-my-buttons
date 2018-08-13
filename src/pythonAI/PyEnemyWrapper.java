8d6ba072dc7f71969c2c737b7af6ccff72e162b4
Ben Hansen <nopika99@gmail.com>
2018-08-13 12:15:45 -0600

    Finishing and cleaning up PyEnemyWrapper

--- src/pythonAI/PyEnemyWrapper.java
+++ src/pythonAI/PyEnemyWrapper.java
@@ -4,17 +4,18 @@ import characters.Character;
 import program.Hitbox;
 import program.Projectile;
 
-class PyEnemyWrapper implements pythonAI.interfaces.EnemyInterface {
+class PyEnemyWrapper implements pythonAI.interfaces.EnemyInterface
+{
 	public Character m_character;
 	
-	public void setCharacter(Character p_character) {
+	public void setCharacter(Character p_character)
+	{
 		m_character = p_character;
 	}
 	
 	@Override
 	public float getX()
 	{
-		// TODO Auto-generated method stub
 		return (float) m_character.getBody().getWorldCenter().x;
 	}
 
@@ -27,52 +28,60 @@ class PyEnemyWrapper implements pythonAI.interfaces.EnemyInterface {
 	@Override
 	public float getHitstun()
 	{
-		// TODO Auto-generated method stub
 		return m_character.peekState().getTimer();
 	}
 	
 	@Override
-	public int getDamage() {
+	public int getDamage()
+	{
 		return m_character.getDamage();
 	}
 
 	@Override
-	public double getXVel() {
+	public double getXVel()
+	{
 		return m_character.getBody().getLinearVelocity().x;
 	}
 
 	@Override
-	public double getYVel() {
+	public double getYVel()
+	{
 		return m_character.getBody().getLinearVelocity().y;
 	}
 	
 	@Override
-	public String getName() { return m_character.getName(); }
+	public String getName()
+	{
+		return m_character.getName();
+	}
 
 	@Override
-	public boolean boosted() {
+	public boolean boosted()
+	{
 		return m_character.getKbooster() > 1;
 	}
 
 	@Override
-	public String currentStateName() {
+	public String currentStateName()
+	{
 		return m_character.peekState().getName();
 	}
 
 	@Override
-	public float currentStateDurationLeft() {
+	public float currentStateDurationLeft()
+	{
 		return m_character.peekState().getTimer();
 	}
 
 	@Override
-	public Hitbox[] getHitboxes() {
-		// TODO Auto-generated method stub
-		return null;
+	public Hitbox[] getHitboxes()
+	{
+		return m_character.getHitboxes();
 	}
 
 	@Override
-	public Projectile[] getProjectiles() {
-		// TODO Auto-generated method stub
-		return null;
+	public Projectile[] getProjectiles()
+	{
+		return (Projectile[]) m_character.getProjectiles();
 	}
 }
